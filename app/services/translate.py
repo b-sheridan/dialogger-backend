55ff50a6d1074dd414cd92492040562b39c71be9
Blake Sheridan <blake.m.sheridan@gmail.com>
2025-11-20 18:15:23 +0000

    add stream_ocr_and_translation()

--- app/services/translate.py
+++ app/services/translate.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+from base64 import b64encode
 from typing import AsyncGenerator
 
 from openai import AsyncOpenAI
 
 from app.config import OPENAI_API_KEY, OPENAI_MODEL
 from app.models import Entry
+
+SEPARATOR = '\x1f'
 
 client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
@@ -36,3 +39,61 @@ async def stream_translation(entry: Entry) -> AsyncGenerator[str]:
                 return  # done
             case _:
                 raise NotImplementedError(event)
+
+
+async def stream_ocr_and_translation(entry: Entry) -> AsyncGenerator[tuple[str, str]]:
+    assert entry.image_path, 'Entry must have been created from an image'
+
+    prompt = [
+        f'Extract and translate text from an image from {entry.scene.project.name}.',
+        f'Between the extracted and translated text just insert this separator: {SEPARATOR}.',
+        'The image is included with this request.',
+        'Send me ONLY the extracted text and translated text; do not talk to me.',
+    ]
+    if len(entry.scene.entries) > 1:
+        prompt.append('Previous scene context:')
+        for number, prev_entry in enumerate(entry.scene.entries[:-1], 1):
+            prompt.append(f'{number}. {prev_entry.text}')
+
+    with open(entry.image_path, 'rb') as image_file:
+        base64_image = b64encode(image_file.read()).decode('utf-8')
+
+    stream = await client.responses.create(
+        model=OPENAI_MODEL,
+        stream=True,
+        input=[
+            {
+                'role': 'user',
+                'content': [
+                    {
+                        'type': 'input_text',
+                        'text': '\n'.join(prompt)
+                    },
+                    {
+                        'type': 'input_image',
+                        'image_url': f'data:image/jpeg;base64,{base64_image}',
+                    },
+                ]
+            },
+        ],
+    )
+
+    text = ''
+    translation = ''
+    sequence = 0
+    async for event in stream:
+        match event.type:
+            case 'response.output_text.delta':
+                if event.delta == SEPARATOR:
+                    sequence += 1
+                elif sequence == 0:
+                    text += event.delta
+                elif sequence == 1:
+                    translation += event.delta
+                else:
+                    raise RuntimeError(f'invalid sequence {sequence}')
+                yield (text, translation)
+            case 'response.completed':
+                break
+            case _:
+                pass
--- tests/test_translate.py
+++ tests/test_translate.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import pytest
 
-from app.services.translate import get_prompt, stream_translation
+from app.models import Entry, Scene, Project
+from app.services.translate import get_prompt, stream_ocr_and_translation, stream_translation
 
 
 def test_get_prompt_doesnt_barf(example_scene):
@@ -16,3 +17,18 @@ def test_get_prompt_doesnt_barf(example_scene):
 async def test_stream_translation(example_scene):
     async for token in stream_translation(example_scene.entries[-1]):
         assert token
+
+
+@pytest.mark.asyncio
+@pytest.mark.openai
+async def test_stream_ocr_and_translation():
+    project = Project(name='Xenogears')
+    scene = Scene(project=project)
+    entry = Entry(scene=scene, image_path='tests/data/xenogears-320-240.png')
+
+    async for (text, translation) in stream_ocr_and_translation(entry):
+        entry.text = text
+        entry.translation = translation
+
+    assert entry.text
+    assert entry.translation
